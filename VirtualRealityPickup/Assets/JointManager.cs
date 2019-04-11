df8bfe5dd87a313c7605102cf61a86c625ba8e9b
Leon Macleod <leondeanmacleod@gmail.com>
2019-04-11 13:24:06 +1200

    Objects can now be held
    In this build if you touch an object with your right hand you will be holding it.

---
+++ VirtualRealityPickup/Assets/JointManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointManager : MonoBehaviour
+{
+
+    public FixedJoint joint;
+    public GameObject rigidCube;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == rigidCube)
+        {
+            Debug.Log("Contact");
+
+            joint.connectedBody = rigidCube.GetComponent<Rigidbody>();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
